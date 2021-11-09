bab0d67d36d06fee78241b954afad5014d4703f4
Tissue94 <tissue2979@naver.com>
2021-11-09 17:39:40 +0900

    Initial commit

---
+++ test.js
@@ -0,0 +1,39 @@
+
+/*const readLine = require('readline-sync')
+
+console.log("ODD EVEN")
+const answer = readLine.prompt()
+
+console.log(answer)
+*/
+const A = Math.PI * Math.pow(20, 2)
+
+const B = Math.PI * Math.pow(10, 2)
+
+let gap = A - B
+
+gap = gap.toFixed(2)
+
+console.log(gap)
+
+/* //주어진 데이터
+const r1 = 10
+ronst r2 = 20
+
+//원의 넓이
+const area1 = Math.pow(r1,2) * Math.PI
+const area2 = Math.pow(r2,2) * Math.PI
+
+let gap = area1 - area2
+
+// if 삼항연산자 - 음수가 나올때
+
+if(gap < 0){
+    gap = gap * -1
+
+gap = gap < 0 ? gap * -1 : gap
+
+console.log("이 도형의 넓이는 " + gap)
+
+console.log('이 도형의 넓이는 ${gap} 입니다')
+ */
